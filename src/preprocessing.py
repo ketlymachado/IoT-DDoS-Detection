d8fab45880b6171da32b0a4fb0c8af6783f08b5d
Kétly G. Machado <ketly.machado@gmail.com>
2021-10-27 16:57:09 -0300

    Adding the final version of the BoT-IoT EDA and the modifications in the preprocessing.py to meet the needs of the EDA performed. Also added some minor organization changes.
    Signed-off-by: Kétly G. Machado <ketly.machado@gmail.com>

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 #########################################################################
 #                                                                       #
-# Project           :                                                   #
+# Project           : IoT DDoS Detection Based on Ensemble Methods for  #
+#                     Evolving Data Stream Classification               #
 #                                                                       #
 # Program name      : preprocessing.py                                  #
 #                                                                       #
@@ -10,15 +11,44 @@
 #                     generates a CSV file containing all the normal    #
 #                     traffic instances and a sample (or all, depending #
 #                     on the percentage inputed) of the DDoS attack     #
-#                     traffic instances.                                #
+#                     (except HTTP) traffic instances.                  #
 #                                                                       #
 #########################################################################
-
 
 import argparse
 import csv
 import random
+import re
 from progress.bar import Bar
+
+# Splits IPv4 into 4 new features, one per part of the address
+# Sets IPv6 features to -1
+def ipv4(row, index):
+    ipv4 = row[index].split(".")
+    for e in ipv4:
+        row.append(e)
+    for _ in range(8):
+        row.append(-1)
+
+# Splits IPv6 into 8 new features, one per part of the address
+# Sets IPv4 features to -1
+def ipv6(row, index):
+    ipv6 = row[index].split(":")
+    for i, e in enumerate(ipv6):
+        if e == "":
+            ipv6.remove("")
+            for j in range(0, 8 - len(ipv6)):
+                ipv6.insert(i + j, "0")
+            break
+    for i, e in enumerate(ipv6):
+        ipv6[i] = int(e, 16)
+    if len(ipv6) < 8:
+        for j in range(0, 8 - len(ipv6)):
+            ipv6.append(-1)
+    for i in range(4):
+        row.append(-1)
+    for e in ipv6:
+        row.append(e)
 
 # Parser to argument p, which determines the percentage of attack instances 
 # to be considered in the preprocessing of the BoT-IoT dataset
@@ -26,16 +56,24 @@ parser = argparse.ArgumentParser(description = "Preprocessing")
 parser.add_argument("-p", action = "store", dest = "p", 
                     default = "100", required = True,
                     help = "Percentage of attack instances to be considered.")
+# Argument s is used only for tests to add and extra string to the resulting file name
+parser.add_argument("-s", action = "store", dest = "s", 
+                    default = "", required = False,
+                    help = "String to add to the resulting file name.")
 
 args = parser.parse_args()
 
 # Adds the header to the data
 with open("../raw-data/UNSW_2018_IoT_Botnet_Dataset_Feature_Names.csv") as features:
-    with open("../processed-data/botiot-" + args.p + ".csv", "w") as botiot:
-        botiot.write(features.read() + "\n")
+    with open("../processed-data/CSV/botiot-" + args.p + args.s + ".csv", "w") as botiot:
+        header = features.read()[:-1]
+        # Source and destination IP addresses extra features
+        header = header + ",sipv4_pos1,sipv4_pos2,sipv4_pos3,sipv4_pos4,sipv6_pos1,sipv6_pos2,sipv6_pos3,sipv6_pos4,sipv6_pos5,sipv6_pos6,sipv6_pos7,sipv6_pos8"
+        header = header + ",dipv4_pos1,dipv4_pos2,dipv4_pos3,dipv4_pos4,dipv6_pos1,dipv6_pos2,dipv6_pos3,dipv6_pos4,dipv6_pos5,dipv6_pos6,dipv6_pos7,dipv6_pos8\n"
+        botiot.write(header)
     botiot.close()
 
-with open("../processed-data/botiot-" + args.p + ".csv", "a", newline="") as botiot:
+with open("../processed-data/CSV/botiot-" + args.p + args.s + ".csv", "a", newline="") as botiot:
 
     spamwriter = csv.writer(botiot, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
 
@@ -44,8 +82,12 @@ with open("../processed-data/botiot-" + args.p + ".csv", "a", newline="") as bot
     count_normal = 0
     count_ddos = 0
     
+    # Defines the pattern for IPv4
+    pattern = re.compile("^(\d{1,3}\.){3}\d{1,3}$")
+
     # Processing bar to follow the execution status
     bar = Bar("Processing", max = 74)
+
     # Reads each one of the files from the original dataset
     for i in range(1, 75):
         with open("../raw-data/UNSW_2018_IoT_Botnet_Dataset_" + str(i) + ".csv") as data:
@@ -53,22 +95,26 @@ with open("../processed-data/botiot-" + args.p + ".csv", "a", newline="") as bot
             csv_reader = csv.reader(data, delimiter=",")
 
             for row in csv_reader:
-                # We consider the instance only if it is either normal or DDoS attack traffic
-                # 08/30 Test - Considering reconnaissance attacks as normal traffic
-                #if row[33] == "Normal" or row[33] == "Reconnaissance" or (row[33] == "DDoS" and row[34] != "HTTP"):
-                if row[33] == "Normal" or (row[33] == "DDoS" and row[34] != "HTTP"):
-                    #if row[33] == "Reconnaissance":
-                        #row[32] = "0"
-                    if row[33] == "Normal": #or row[33] == "Reconnaissance":
+                # Source and destination IP addresses technique to convert from categorical to numerical
+                    # Done at this point to avoid deal with it in the memory while performing the EDA
+                if (pattern.match(row[4])):
+                    ipv4(row, 4)
+                else:
+                    ipv6(row, 4)
+                if (pattern.match(row[6])):
+                    ipv4(row, 6)
+                else:
+                    ipv6(row, 6)
+                # We consider the instance only if it is either normal or DDoS attack traffic (except HTTP)
+                if row[33] == "Normal":
+                    spamwriter.writerow(row)
+                    count_normal = count_normal + 1
+                elif (row[33] == "DDoS" and row[34] != "HTTP"):
+                    # Randomly chooses to include or not the attack instance based on the chosen percentage
+                    x = random.random()
+                    if x < (float(args.p) / 100.00):
                         spamwriter.writerow(row)
-                        count_normal = count_normal + 1
-                    else:
-                        x = random.random()
-                        # Randomly chooses to include or not the attack instance based on the chosen percentage
-                        if x < (float(args.p) / 100.00):
-                            spamwriter.writerow(row)
-                            count_ddos = count_ddos + 1
-                    
+                        count_ddos = count_ddos + 1
         bar.next()
     bar.finish()
 
