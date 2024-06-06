48c69135fc1c87e015adde0c596279fcdd3bb5fa
Kétly G. Machado <ketly.machado@gmail.com>
2024-06-06 14:36:51 -0300

    refactor: data set info

--- src/extra_dataset_info.py
+++ src/extra_dataset_info.py
@@ -1,36 +1,40 @@
 import csv
+import os
+from pathlib import Path
 
-COUNT_NORMAL = 0
-COUNT_DDOS = 0
-COUNT_TNORMAL = 0
-COUNT_TDDOS = 0
+COUNT_FILE_NORMAL = 0
+COUNT_FILE_DDOS = 0
+COUNT_TOTAL_NORMAL = 0
+COUNT_TOTAL_DDOS = 0
 
 LABEL_COLUMN = 33
 CATEGORY_COLUMN = 34
 
-# Reads each one of the files from the original dataset
+DATASET_FOLDER = os.path.join(Path(__file__).absolute().parent, "../raw-data/BoT-IoT/")
+DATASET_FILE_PREFIX = "UNSW_2018_IoT_Botnet_Dataset_"
+
 for i in range(1, 75):
-    with open(
-        "../raw-data/BoT-IoT/UNSW_2018_IoT_Botnet_Dataset_" + str(i) + ".csv",
-        encoding="utf-8",
-    ) as data:
-        csv_reader = csv.reader(data, delimiter=",")
+    original_file = os.path.join(
+        DATASET_FOLDER,
+        DATASET_FILE_PREFIX + str(i) + ".csv",
+    )
+    with open(original_file, encoding="utf-8") as subset:
+        csv_reader = csv.reader(subset, delimiter=",")
 
         for row in csv_reader:
             if row[LABEL_COLUMN] == "Normal":
-                COUNT_NORMAL = COUNT_NORMAL + 1
+                COUNT_FILE_NORMAL = COUNT_FILE_NORMAL + 1
             if row[LABEL_COLUMN] == "DDoS" and row[CATEGORY_COLUMN] != "HTTP":
-                COUNT_DDOS = COUNT_DDOS + 1
+                COUNT_FILE_DDOS = COUNT_FILE_DDOS + 1
 
-        print(f"{i:0.2d}/74")
-        print(f"Total normal instances from file {i} = {COUNT_NORMAL}")
-        print(f"Total DDoS attack instances from file {i} = {COUNT_DDOS}")
+        print(f"Total normal instances from file {i} = {COUNT_FILE_NORMAL}")
+        print(f"Total DDoS attack instances from file {i} = {COUNT_FILE_DDOS}")
         print()
-        COUNT_TNORMAL = COUNT_TNORMAL + COUNT_NORMAL
-        COUNT_TDDOS = COUNT_TDDOS + COUNT_DDOS
-        COUNT_NORMAL = 0
-        COUNT_DDOS = 0
+        COUNT_TOTAL_NORMAL = COUNT_TOTAL_NORMAL + COUNT_FILE_NORMAL
+        COUNT_TOTAL_DDOS = COUNT_TOTAL_DDOS + COUNT_FILE_DDOS
+        COUNT_FILE_NORMAL = 0
+        COUNT_FILE_DDOS = 0
 
-print(f"Total normal instances = {COUNT_TNORMAL}")
-print(f"Total DDoS attack instances = {COUNT_TDDOS}")
+print(f"Total normal instances = {COUNT_TOTAL_NORMAL}")
+print(f"Total DDoS attack instances = {COUNT_TOTAL_DDOS}")
 print("\n")
