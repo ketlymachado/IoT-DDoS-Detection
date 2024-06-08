643edd30bc9f8cda3b88b2ccf4b4f3d2100c4e5c
Kétly G. Machado <ketly.machado@gmail.com>
2024-06-08 20:47:06 -0300

    refactor: adjusting data balancing to consume less memory

--- src/data_balancing.py
+++ src/data_balancing.py
@@ -6,6 +6,9 @@ from sklearn.neighbors import NearestNeighbors
 from tqdm import tqdm
 from tap import Tap
 
+header = []
+SYNTHETIC_INSTANCES_FILENAME = ""
+
 
 class ArgumentParser(Tap):
     """Class defines arguments typing."""
@@ -27,11 +30,24 @@ def parse() -> ArgumentParser:
     return arguments
 
 
-def create_info_file(filename, normal_instances, attack_instances):
+def create_info_file(
+    filename, minority_class_length, majority_class_length, minority_class_label
+):
     """Function creates info file about the balanced data"""
     info_filename = os.path.join(
         os.path.dirname(filename),
         os.path.splitext(os.path.basename(filename))[0] + "-balanced-info.txt",
+    )
+
+    normal_instances = (
+        majority_class_length
+        if minority_class_label == "attack"
+        else minority_class_length
+    )
+    attack_instances = (
+        majority_class_length
+        if minority_class_label == "normal"
+        else minority_class_length
     )
 
     with open(info_filename, "w", encoding="utf-8") as balanced_info_file:
@@ -48,21 +64,40 @@ def create_info_file(filename, normal_instances, attack_instances):
     balanced_info_file.close()
 
 
-def create_balanced_file(filename, header, instances, systhetic_instances):
+def create_balanced_file(filename):
     """Function creates the balanced data file"""
 
     os.rename(filename, filename + ".old")
 
     with open(filename, "w", newline="", encoding="utf-8") as balanced_file:
         balanced_file.writelines(header)
-        balanced_file.writelines(
-            "\n".join([",".join(instance) for instance in instances]) + "\n"
-        )
-        balanced_file.writelines(
-            "\n".join([",".join(instance) for instance in systhetic_instances]) + "\n"
+
+        spamwriter = csv.writer(
+            balanced_file,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL,
         )
 
+        with open(filename + ".old", encoding="utf-8") as instances_file:
+            reader = csv.reader(instances_file, delimiter=",")
+
+            for row in reader:
+                if row[0][0] == "@":
+                    continue
+
+                spamwriter.writerow(row)
+        instances_file.close()
+
+        with open(
+            SYNTHETIC_INSTANCES_FILENAME, encoding="utf-8"
+        ) as synthetic_instances_file:
+            reader = csv.reader(synthetic_instances_file, delimiter=",")
+            spamwriter.writerows(reader)
+        synthetic_instances_file.close()
+
     balanced_file.close()
+    os.remove(SYNTHETIC_INSTANCES_FILENAME)
 
 
 def nearest_neighbour(x):
@@ -74,23 +109,31 @@ def nearest_neighbour(x):
     return indices
 
 
-def populate(smote_amount, idx, neighbours, minority_class, number_of_columns):
+def populate(smote_amount, idx, neighbours, minority_class, label):
     """Function to generate the synthetic samples"""
-    synthetic_instances = []
-    while smote_amount > 0:
-        neighbour = choice(neighbours)
-        synthetic_instances.append([])
-        for column_idx in range(number_of_columns):
-            dif = (
-                minority_class[neighbour][column_idx] - minority_class[idx][column_idx]
-            )
-            gap = random()
-            synthetic_instances[-1].append(
-                str(minority_class[idx][column_idx] + gap * dif)
-            )
-        smote_amount = smote_amount - 1
+    with open(
+        SYNTHETIC_INSTANCES_FILENAME, "a", encoding="utf-8"
+    ) as synthetic_instances_file:
+        spamwriter = csv.writer(
+            synthetic_instances_file,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL,
+        )
 
-    return synthetic_instances
+        while smote_amount > 0:
+            neighbour = choice(neighbours)
+            synthetic_instance = []
+
+            for column_idx, value in enumerate(minority_class[idx]):
+                dif = minority_class[neighbour][column_idx] - value
+                gap = random()
+                synthetic_instance.append(value + gap * dif)
+
+            synthetic_instance.append(label)
+            spamwriter.writerow(synthetic_instance)
+            smote_amount = smote_amount - 1
+    synthetic_instances_file.close()
 
 
 def smote(minority_class, smote_percentage, label):
@@ -100,63 +143,47 @@ def smote(minority_class, smote_percentage, label):
         if smote_percentage % 100 >= 50
         else floor(smote_percentage / 100)
     )
-    number_of_columns = len(minority_class[0])
     neighbour_indices = nearest_neighbour(minority_class)
-    synthetic_instances = []
 
-    for idx, neighbours in enumerate(neighbour_indices):
-        synthetic_instances.extend(
-            populate(
-                smote_amount,
-                idx,
-                neighbours,
-                minority_class,
-                number_of_columns,
-            )
+    for idx, neighbours in tqdm(enumerate(neighbour_indices)):
+        populate(
+            smote_amount,
+            idx,
+            neighbours,
+            minority_class,
+            label,
         )
 
-    for instance in synthetic_instances:
-        instance.append(label)
-
-    return synthetic_instances
+    return smote_amount * len(minority_class)
 
 
-def initial_compute(filename):
-    """Function initializes control variables and returns it"""
+def get_minority_class(filename):
+    """Function calculates minority class and returns information on it"""
     normal = []
     attack = []
-    instances = []
-    header = []
 
     with open(filename, encoding="utf-8") as file:
         reader = csv.reader(file, delimiter=",")
 
-        for row in reader:
+        for row in tqdm(reader):
             if row[0][0] == "@":
                 header.append("".join(row) + "\n")
                 continue
 
-            instances.append(row)
-            instance = []
-
-            for idx, value in enumerate(row):
-                is_last_column = idx == len(row) - 1
-                if is_last_column:
-                    continue
-                instance.append(float(value))
-
             if row[-1] == "attack":
-                attack.append(instance)
+                attack.append([float(column) for column in row[:-1]])
             else:
-                normal.append(instance)
+                normal.append([float(column) for column in row[:-1]])
     file.close()
 
-    return normal, attack, instances, header
+    if len(attack) > len(normal):
+        return normal, "normal", len(normal) / len(attack), len(attack)
+
+    return attack, "attack", len(attack) / len(normal), len(normal)
 
 
-def execute(folder):
-    """Function uses SMOTE techinique to balance class distribution"""
-
+def get_files(folder):
+    """Function returns all arff files from folder"""
     files = []
 
     for subfolder in os.listdir(folder):
@@ -165,33 +192,37 @@ def execute(folder):
             if os.path.isfile(file_path) and file_path.endswith(".arff"):
                 files.append(file_path)
 
+    return files
+
+
+def execute(folder):
+    """Function uses SMOTE techinique to balance class distribution"""
+
+    files = get_files(folder)
+
     print("\nData balancing started...\n")
 
     for filename in tqdm(files):
-        normal, attack, instances, header = initial_compute(filename)
+        # pylint: disable=global-statement
+        global SYNTHETIC_INSTANCES_FILENAME
+        SYNTHETIC_INSTANCES_FILENAME = os.path.join(
+            os.path.dirname(filename), "synthetic_instances.arff"
+        )
 
-        minority_class = normal if len(attack) > len(normal) else attack
-        majority_class = normal if len(normal) > len(attack) else attack
-        minority_class_label = "normal" if len(attack) > len(normal) else "attack"
-        scale = len(minority_class) / len(majority_class)
+        minority_class, minority_class_label, scale, majority_class_length = (
+            get_minority_class(filename)
+        )
 
         if scale < 0.5:
-            synthetic_instances = smote(
+            synthetic_instances_length = smote(
                 minority_class, 100 / scale, minority_class_label
             )
-            create_balanced_file(filename, header, instances, synthetic_instances)
+            create_balanced_file(filename)
             create_info_file(
                 filename,
-                (
-                    len(normal) + len(synthetic_instances)
-                    if minority_class_label == "normal"
-                    else len(normal)
-                ),
-                (
-                    len(attack) + len(synthetic_instances)
-                    if minority_class_label == "attack"
-                    else len(attack)
-                ),
+                majority_class_length,
+                len(minority_class) + synthetic_instances_length,
+                minority_class_label,
             )
 
     print("\n...Data balancing finished\n")
